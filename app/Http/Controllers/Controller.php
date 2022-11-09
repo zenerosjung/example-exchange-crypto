601e291a399fdfd4b68ac663c91da3e9efe855ee
zeneros <benz_8500_@hotmail.com>
2022-11-09 23:43:41 +0700

    - add config DB

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -10,4 +10,14 @@ use Illuminate\Routing\Controller as BaseController;
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    public function index()
+    {
+        return view('index');
+    }
+
+    public function login()
+    {
+        return view('login');
+    }
 }
